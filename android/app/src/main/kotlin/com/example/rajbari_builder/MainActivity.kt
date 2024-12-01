f262ccc188c47580696985f3980be93b32b2b11d
MD Emon Miah <mdemonmiah@MDs-MacBook-Pro.local>
2024-12-01 10:30:33 +0600

    update

---
+++ android/app/src/main/kotlin/com/example/rajbari_builder/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.rajbari_builder
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/utils/app_constant.dart
@@ -0,0 +1,4 @@
+class AppConstant {
+  static const String appFont = 'Arial';
+  static const String appName = 'Rajbari Builder';
+}
